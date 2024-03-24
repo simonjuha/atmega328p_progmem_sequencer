8ba2362fc11e78469d87ba73c65c450a198c9169
SImon <33484974+simonjuha@users.noreply.github.com>
2024-03-24 16:43:01 +0100

    Initial commit

---
+++ include/step.hpp
@@ -0,0 +1,49 @@
+#pragma once
+#include <stdint.h>
+#include <stdlib.h>
+
+class Step{
+    public:
+    Step(uint8_t stepNumber) : _stepNumber(stepNumber){
+        _sampleRate = rand() % 255;
+    }
+    Step() : _stepNumber(0){
+        _sampleRate = rand() % 255;
+    }
+
+    uint8_t getSampleRate(){
+        return _sampleRate;
+    }
+
+    void setStepNumber(uint8_t stepNumber){
+        _stepNumber = stepNumber;
+    }
+
+    uint8_t getStepNumber(){
+        return _stepNumber;
+    }
+
+    uint8_t getStartOffset(){
+        return _startOffset;
+    }
+
+    uint8_t getLength(){
+        return _length;
+    }
+
+    void setLength(uint8_t length){
+        _length = length;
+    }
+
+    void setState(bool state){
+        _active = state;
+    }
+
+    private:
+        bool    _active = true;
+        uint8_t _sampleRate;
+        uint8_t _startOffset = 0;
+        uint8_t _length;
+        uint8_t _stepNumber;
+
+};
---
+++ include/stepper.hpp
@@ -0,0 +1,73 @@
+#pragma once
+#include <stdint.h>
+#include <stdlib.h>
+#include "step.hpp"
+
+#define STEPPER_STEPS_MAX 8 // max steps of a single sequence
+#define STEPPER_STEPS_DIV 8 // ministeps per step
+#define STEPPER_STEPS_TOT STEPPER_STEPS_MAX * STEPPER_STEPS_DIV // total steps
+
+// ON / OFF / CONTINUE - gates
+enum StepState{
+    ON,
+    OFF,
+    CONTINUE
+};
+
+class Stepper {
+public:
+    Stepper(){
+        for(uint8_t i = 0; i < STEPPER_STEPS_MAX; i++){
+            _steps[i].setStepNumber(i*STEPPER_STEPS_DIV);
+            _steps[i].setLength(STEPPER_STEPS_DIV/2);
+        }
+    }
+
+    StepState step(){
+
+        _step++;
+        if(_step >= STEPPER_STEPS_TOT){
+            _step = 0;
+        }
+        
+        Step* currentStep = &_steps[_stepIndex];
+
+
+        // step inactive (OFF)
+        if(currentStep->getStepNumber() + currentStep->getStartOffset() + currentStep->getLength() == _step){
+            _stepIndex++;
+            if(_stepIndex >= STEPPER_STEPS_MAX){
+                _stepIndex = 0;
+            }
+            return OFF; // sampled stops playing
+        }
+
+        // step active (ON)
+        if(currentStep->getStepNumber() + currentStep->getStartOffset() == _step){
+
+            return ON; // sample is played
+        }
+
+        return CONTINUE;
+    }
+
+    void setTempo(uint8_t tempo){
+        _tempo = tempo;
+    }
+
+    uint8_t getStepSampleRate(){
+        return _steps[_stepIndex].getSampleRate();
+    }
+
+
+
+private:
+    uint8_t _tempo;
+    uint8_t _step = 0;
+    Step _steps[STEPPER_STEPS_MAX];
+
+    uint8_t _stepIndex = 0;
+};
+
+
+
