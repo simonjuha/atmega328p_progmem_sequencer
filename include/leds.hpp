999c3fe20dbd7734560e4aae9151aaf51ceee3d9
SImon <33484974+simonjuha@users.noreply.github.com>
2024-04-11 15:37:17 +0200

    consistent LED indication + refactoring

--- include/leds.hpp
+++ include/leds.hpp
@@ -39,45 +39,66 @@ class Leds{
             }
         }
 
-        void setSpan(int end){
+        void setAllLedsOff() {
             for(int i = 0; i < N_LEDS; i++){
                 leds[i] = _off;
             }
+        }
+        
+        void setSelectedLedColor() {
+            int selected = _stepper.getSelectedStep();
+            bool nextStepActive = _stepper.getNextStepActive();
+            leds[selected] = mixColors(leds[selected], _onStepColor);
+            // if the next step is active, show it
+            if(nextStepActive && selected == _stepper.getStepIndex()) {
+                leds[selected] = mixColors(leds[selected], _activeColor);
+            }
+        }
+        
+        void setSpan(int end){
+            setAllLedsOff();
+            // set the selected led
             for(int i = 0; i < end; i++){
                 leds[i] = _selectColor;
             }
+            setSelectedLedColor();
             refresh();
         }
+        
         void stepping(){
             int selected = _stepper.getSelectedStep();
             int activeStep = _stepper.getStepIndex();
             bool nextStepActive = _stepper.getNextStepActive();
             
+            // show the steps that are active
             for(int i = 0; i < N_LEDS; i++){
                 if(_stepper.getStep(i)->getState()) {
-                    leds[i] = _deactivatedColor;
+                    leds[i] = _onStepColor;
                 } else {
                     leds[i] = _off;
                 }
             }
             
+            // if the next step is active, show it
             if(nextStepActive) {
                 leds[activeStep] = _activeColor;
             }
             
+            // show the selected step
             leds[selected] = _selectColor;
             
+            // if the selected step is the active step, show it
             if(nextStepActive && activeStep == selected) {
-                leds[selected] = _activeAndSelectedColor;
+                leds[selected] = mixColors(_activeColor, _selectColor);
             }
             refresh();
         }
+        
         void setFromCenter(unsigned int index){
-            for(int i = 0; i < N_LEDS; i++){
-                leds[i] = _off;
-            }
+            setAllLedsOff();
             int centeredValue = CENTER_LED + index;
-
+        
+            leds[CENTER_LED] = _selectColor;
             if(centeredValue > CENTER_LED){
                 for(int i = CENTER_LED+1; i <= centeredValue; i++){
                     leds[i] = _selectColor;
@@ -88,23 +109,28 @@ class Leds{
                     leds[i] = _selectColor;
                 }
             }
-
-            leds[CENTER_LED] = _defaultColor;
-            leds[centeredValue] = _activeColor;
+        
+            setSelectedLedColor();
             refresh();
         }
-
 
     private:
         void refresh(){
             ws2812_setleds(leds, N_LEDS);
         }
+        cRGB mixColors(const cRGB &color1,const cRGB &color2){
+            cRGB result;
+            // OR colors together:
+            result.r = color1.r | color2.r;
+            result.g = color1.g | color2.g;
+            result.b = color1.b | color2.b;
+            return result;
+        }
         ws2812_RGB_t leds[N_LEDS];
         const cRGB _selectColor = {0, 0, 1};
         const cRGB _activeColor = {0, 1, 0};
-        const cRGB _activeAndSelectedColor = {1, 1, 0};
         const cRGB _defaultColor = {1, 1, 1};
-        const cRGB _deactivatedColor = {1, 0, 0};
+        const cRGB _onStepColor = {1, 0, 0};
         const cRGB _off = {0, 0, 0};
         bool _doChange = false;
 
